e810a3673e08211365c3b14c822fc0c17a2c7f0e
robertuptc <robertuptc@gmail.com>
2025-03-03 15:43:47 -0500

    python problem finished

--- python/factorial.py
+++ python/factorial.py
@@ -1,2 +1,11 @@
 def factorial(num):
-	# your code here
+	# USING FOR-LOOP
+	answer = 1
+	for x in range(num, 0, -1):
+		answer *= x
+	return answer
+
+	# USING RECURSION
+    # if num == 0 or num == 1:
+    #     return 1
+    # return num * factorial(num - 1)
--- python/factorial_spec.py
+++ python/factorial_spec.py
@@ -1,6 +1,8 @@
 from factorial import factorial
 
-print(factorial(8) == 40320)
-print(factorial(18) == 6402373705728000)
-print(factorial(45) == 119622220865480194561963161495657715064383733760000000000)
+print(factorial(4))
+# print(factorial(0) == 1)
+# print(factorial(8) == 40320)
+# print(factorial(18) == 6402373705728000)
+# print(factorial(45) == 119622220865480194561963161495657715064383733760000000000)
 # Test how high of a number your program can calculate. Can you push it further?
